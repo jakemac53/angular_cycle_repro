c9a4f1d75d78c3a2c3409fdfc5cb1071cab783f7
Jacob MacDonald <jakemac@google.com>
2016-04-18 14:21:13 -0700

    initial commit

---
+++ a/lib/app_component.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:b/app_component.dart' as b;
+import 'package:angular2/core.dart';
+
+@Component(selector: 'my-app', templateUrl: 'app_component.html')
+class AppComponent {}
